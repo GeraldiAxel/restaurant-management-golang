3df7c2117cd4c25667b5d825325f130f21728692
GeraldiAxel <97270723+GeraldiAxel@users.noreply.github.com>
2025-04-20 14:02:40 +0700

    add models

--- models/noteModels.go
+++ models/noteModels.go
@@ -1,1 +1,17 @@
 package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Note struct {
+	ID         primitive.ObjectID `bson:"_id"`
+	Text       string             `json:"text"`
+	Title      string             `json:"title"`
+	Created_at time.Time          `json:"created_at"`
+	Updated_at time.Time          `json:"updated_at"`
+	Deleted_at *time.Time         `json:"deleted_at"`
+	Note_id    string             `json:"note_id"`
+}
